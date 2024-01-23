e9210730aaedbfb41550b1d7b95f8fec73ee5d19
Achala Srivastava <119436810+AchalaAS@users.noreply.github.com>
2024-01-23 10:43:10 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// PORTFOLIO 
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+
+  //NAVBAR
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+});
